d429466b0bffbfdf10cfb8abeb49b2532a553223
sugarfreeva <asweet1983@gmail.com>
2015-11-29 08:11:22 -0500

    problem solved

--- Deg2DMS.cpp
+++ Deg2DMS.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
+#include <sstream>
 
 using namespace std;
 
@@ -9,7 +11,33 @@ int main(int argc, char* argv[])
 
     if(inputFile.is_open())
     {
-        
+        string line;
+
+        double input;
+        double temp;
+
+        int deg;
+        int min;
+        int sec;
+
+        while(getline(inputFile,line))
+        {
+            istringstream ss(line);
+
+            ss >> input;
+
+            deg = int(input);
+
+            temp = input - deg;
+
+            min = int(temp * 60);
+
+            temp = (temp*60)-min;
+
+            sec = int(temp * 60);
+
+            cout << deg << "." << setw(2) << setfill('0') << min << "\'" << setw(2) << setfill('0') << sec << "\"" << endl;
+        }
     }
     else
     {
